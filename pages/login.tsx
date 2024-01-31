3cf0c33b2b65bf2cf65ebbda00c3e7fdc1ecfe76
Gayathri Yedavilli <gyedavil@uci.edu>
2024-01-31 11:20:42 -0800

    added very basic styling to login stuff

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,14 +14,15 @@ const Login = () => {
     return (
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
             <div >
-                <h1 style={{textAlign: 'center', marginBottom: '4vh'}}>OpusAI Login</h1>
+                <h1 style={{textAlign: 'center', marginBottom: '4vh', fontSize: '4.5rem'}}>OpusAI Login</h1>
             </div>
             <div>
                 <Link href="/api/login">
-                    <button style={{ fontSize: '3vh'}}>Log in with Google</button>
+                    <button style={{ fontSize: '3vh', padding: '15px 30px', borderRadius: '8px', background: '#007bff', color: '#fff'}}>Log in with Google</button>
                 </Link>
             </div>
         </div>
     );
 };
+
 export default Login;
